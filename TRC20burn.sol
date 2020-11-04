5d584f4d6af7b037ea8e8068451ab3a72483dd4b
mrjky76 <73655776+mrjky76@users.noreply.github.com>
2020-11-05 05:01:50 +0800

    Update TRC20burn.sol

--- TRC20burn.sol
+++ TRC20burn.sol
@@ -174,7 +174,7 @@ contract MintableToken is StandardToken, Ownable {
 		balances[_to] = balances[_to].add(_amount);
 		emit Mint(_to, _amount);
 		emit Transfer(address(0), _to, _amount);
-		return false;
+		return true;
     }
 }
 
