188c984557c150987eafa6dd9d5ce264726f6717
mrjky76 <73655776+mrjky76@users.noreply.github.com>
2020-11-05 04:02:53 +0800

    Update TRC20burn.sol

--- TRC20burn.sol
+++ TRC20burn.sol
@@ -102,6 +102,41 @@ contract StandardToken {
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
+}
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+ constructor() public  {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
 
 contract MintableToken is StandardToken, Ownable {
     event Mint(address indexed to, uint256 amount);
