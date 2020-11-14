fffb76b9f5ce711a428aa56dc9cab23ff97acef9
mrjky76 <73655776+mrjky76@users.noreply.github.com>
2020-11-15 00:02:27 +0800

    Update multibulksend.sol

--- multibulksend.sol
+++ multibulksend.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.4.25;
 
-contract ERC20 {
+contract TRC20 {
     function transfer(address _recipient, uint256 amount) public;
 }       
 contract MultiTransfer {
-    function multiTransfer(ERC20 token, address[] _addresses, uint256 amount) public {
+    function multiTransfer(TRC20 token, address[] _addresses, uint256 amount) public {
         for (uint256 i = 0; i < _addresses.length; i++) {
             token.transfer(_addresses[i], amount);
         }
